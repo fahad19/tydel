b589a03badc6e497bed17e9b16b9e78762d721a8
Fahad Ibnay Heylaal <fahad19@gmail.com>
2016-07-17 23:49:46 +0200

    models: fix for multiple instances' state.

--- src/createModel.js
+++ src/createModel.js
@@ -17,76 +17,76 @@ export default function createModel(schema = {}, actions = {}) {
     throw new ActionError('conflicting action and schema: ' + commonKeysList);
   }
 
-  let applySchema = () => {};
-  let attributes = {};
-
-  function defineAttributes(context, attrs, pathPrefix = '') {
-    _.each(attrs, (v, k) => {
-      Object.defineProperty(context, k, {
-        get() {
-          const getPath = pathPrefix
-            ? pathPrefix + '.' + k
-            : k;
-
-          if (isModel(v)) {
-            return v;
-          }
-
-          const result = _.clone(_.get(attributes, getPath));
-
-          if (_.isPlainObject(result)) {
-            defineAttributes(result, result, getPath);
-          }
-
-          return result;
-        }
-      });
-    });
-  }
-
-  function defineActions(context, actions) {
-    _.each(actions, (action, actionName) => {
-      attributes[actionName] = action;
-
-      Object.defineProperty(context, actionName, {
-        get() {
-          return function (...args) {
-            const output = action.bind(attributes)(...args);
-
-            applySchema(attributes);
-
-            return output;
-          };
-        }
-      });
-    });
-  }
-
   function Model(givenAttributes = {}) {
+    let applySchema = () => {};
+    let attributes = {};
+
+    function defineAttributes(context, attrs, pathPrefix = '') {
+      _.each(attrs, (v, k) => {
+        Object.defineProperty(context, k, {
+          get() {
+            const getPath = pathPrefix
+              ? pathPrefix + '.' + k
+              : k;
+
+            if (isModel(v)) {
+              return v;
+            }
+
+            const result = _.clone(_.get(attributes, getPath));
+
+            if (_.isPlainObject(result)) {
+              defineAttributes(result, result, getPath);
+            }
+
+            return result;
+          }
+        });
+      });
+    }
+
+    function defineActions(context, actions) {
+      _.each(actions, (action, actionName) => {
+        attributes[actionName] = action;
+
+        Object.defineProperty(context, actionName, {
+          get() {
+            return function (...args) {
+              const output = action.bind(attributes)(...args);
+
+              applySchema(attributes);
+
+              return output;
+            };
+          }
+        });
+      });
+    }
+
     applySchema = Types.object.of(schema);
     attributes = applySchema(givenAttributes);
 
     defineAttributes(this, attributes);
     defineActions(this, actions);
+
+    this.toJS = function () {
+      function convertToJS(attrs) {
+        return _.mapValues(attrs, (v, k) => {
+          if (isModel(v)) {
+            return v.toJS();
+          }
+
+          if (_.isPlainObject(v)) {
+            return convertToJS(v);
+          }
+
+          return v;
+        });
+      }
+
+      return convertToJS(attributes);
+    };
   }
-
-  Model.prototype.toJS = function () {
-    function convertToJS(attrs) {
-      return _.mapValues(attrs, (v, k) => {
-        if (isModel(v)) {
-          return v.toJS();
-        }
-
-        if (_.isPlainObject(v)) {
-          return convertToJS(v);
-        }
-
-        return v;
-      });
-    }
-
-    return convertToJS(attributes);
-  };
 
   return Model;
 }

c5cb50e92a3161973ed5c284012cc4e6ded638d4
Fahad Ibnay Heylaal <fahad19@gmail.com>
2016-07-20 23:54:52 +0200

    models: listener methods.

--- src/createModel.js
+++ src/createModel.js
@@ -12,33 +12,58 @@ export default function createModel(schema = {}, methods = {}) {
       super(givenAttributes);
 
       let attributes = {};
+      let listeners = {};
 
       // built-in methods
       Object.defineProperty(this, 'toJS', {
-        get() {
-          return function () {
-            function convertToJS(attrs) {
-              return _.mapValues(attrs, (v, k) => {
-                if (
-                  isModel(v) ||
-                  isCollection(v)
-                ) {
-                  return v.toJS();
-                }
+        value: function () {
+          function convertToJS(attrs) {
+            return _.mapValues(attrs, (v, k) => {
+              if (
+                isModel(v) ||
+                isCollection(v)
+              ) {
+                return v.toJS();
+              }
 
-                if (_.isPlainObject(v)) {
-                  return convertToJS(v);
-                }
+              if (_.isPlainObject(v)) {
+                return convertToJS(v);
+              }
 
-                return v;
-              });
-            }
+              return v;
+            });
+          }
 
-            return convertToJS(attributes);
-          };
+          return convertToJS(attributes);
         }
       });
 
+      Object.defineProperty(this, 'on', {
+        value: function (event, fn) {
+          // sets listener
+        }
+      });
+
+      Object.defineProperty(this, 'trigger', {
+        value: function (event, ...args) {
+          // triggers event
+        }
+      });
+
+      Object.defineProperty(this, 'off', {
+        value: function (event, fn = null) {
+          // unlistens
+        }
+      });
+
+      Object.defineProperty(this, 'destroy', {
+        value: function () {
+          // kills all listeners
+          // destroys self
+        }
+      });
+
+      // parse by schema
       const applySchema = Types.object.of(schema);
       attributes = applySchema(givenAttributes);
 

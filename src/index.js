144dc01200a895b92d30e97a3c1f209cc3610b07
Fahad Ibnay Heylaal <fahad19@gmail.com>
2016-07-23 22:14:50 +0200

    dist generator. (#15)

---
+++ src/index.js
@@ -0,0 +1,18 @@
+/* eslint-disable */
+const ChainableTypes = require('./ChainableTypes').default;
+const chainType = require('./chainType').default;
+const createCollection = require('./createCollection').default;
+const createModel = require('./createModel').default;
+const isCollection = require('./isCollection').default;
+const isModel = require('./isModel').default;
+const Types = rquire('./Types').default;
+
+module.exports = {
+  ChainableTypes,
+  chainType,
+  createCollection,
+  createModel,
+  isCollection,
+  isModel,
+  Types
+};
--- test/collection/Collection.removeAt.spec.js
+++ test/collection/Collection.removeAt.spec.js
@@ -4,7 +4,6 @@ import { expect } from 'chai';
 import Types from '../../src/Types';
 import createModel from '../../src/createModel';
 import createCollection from '../../src/createCollection';
-import isModel from '../../src/isModel';
 
 describe('Collection :: removeFrom()', function () {
   it('removes specific model from collection by index', function () {

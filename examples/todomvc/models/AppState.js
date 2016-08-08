d81eadb9bc9d46ad6e6fe97097e446d7faf7d723
Fahad Ibnay Heylaal <fahad19@gmail.com>
2016-08-08 22:07:04 +0200

    examples: add logger to todomvc

--- examples/todomvc/models/AppState.js
+++ examples/todomvc/models/AppState.js
@@ -1,7 +1,10 @@
 import { Types, createModel } from 'tydel';
+import logger from 'tydel-logger';
 
 import Todos from '../collections/Todos';
 
 export default createModel({
   todos: Types.collection.of(Todos)
-});
+}, {}, [
+  logger
+]);
